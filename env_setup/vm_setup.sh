8fe1fa7756b9d67bf56db1cf083832875019665e
Nahid Mahmud <mahmud.nahid@gmail.com>
2024-08-15 19:14:16 -0400

    fix paths in bash script and conf files

--- env_setup/vm_setup.sh
+++ env_setup/vm_setup.sh
@@ -39,7 +39,7 @@ git clone https://github.com/mahmudnahid/InfrastructureAutomationOnAzure.git
 sudo chmod 755 /var/www/html/ -R
 
 # Setup Virtual Environment
-cd CloudNine
+cd InfrastructureAutomationOnAzure
 sudo `which python3` env_setup/setup.py
 
 # Setup Env variables
@@ -52,34 +52,34 @@ MYSQL_PASSWORD='$MYSQL_PASSWORD'
 AZ_ACCOUNT_NAME='$AZ_ACCOUNT_NAME'
 AZ_ACCOUNT_KEY='$AZ_ACCOUNT_KEY'
 AZURE_SERVICE_BUS_QUEUE_NAME='$AZURE_SERVICE_BUS_QUEUE_NAME'
-AZURE_SERVICE_BUS_CONNECTION_STRING='$AZURE_SERVICE_BUS_CONNECTION_STRING'" | tee /var/www/html/CloudNine/ecommerce/.env
+AZURE_SERVICE_BUS_CONNECTION_STRING='$AZURE_SERVICE_BUS_CONNECTION_STRING'" | tee /var/www/html/InfrastructureAutomationOnAzure/ecommerce/.env
 
 # Load demo data
-cd /var/www/html/CloudNine/ecommerce/
-source /var/www/html/CloudNine/venv/ecom/bin/activate
+cd /var/www/html/InfrastructureAutomationOnAzure/ecommerce/
+source /var/www/html/InfrastructureAutomationOnAzure/venv/ecom/bin/activate
 # Create Tables
-/var/www/html/CloudNine/venv/ecom/bin/python manage.py migrate
+/var/www/html/InfrastructureAutomationOnAzure/venv/ecom/bin/python manage.py migrate
 
 # Insert demo data
-/var/www/html/CloudNine/venv/ecom/bin/python manage.py load_data
+/var/www/html/InfrastructureAutomationOnAzure/venv/ecom/bin/python manage.py load_data
 
 # Upload product images to azure storage 
 # @TODO This command runs multiple times from each VM. Fix it to run it only for the first VM
-/var/www/html/CloudNine/venv/ecom/bin/python manage.py upload_img
+/var/www/html/InfrastructureAutomationOnAzure/venv/ecom/bin/python manage.py upload_img
 
 # Collect static files in azure storage
 # @TODO This command runs multiple times from each VM. Fix it to run it only for the first VM
-/var/www/html/CloudNine/venv/ecom/bin/python manage.py collectstatic --noinput 
+/var/www/html/InfrastructureAutomationOnAzure/venv/ecom/bin/python manage.py collectstatic --noinput 
 
 # Setup SUpervisor
-sudo cp /var/www/html/CloudNine/conf/ecom_supervisor.conf  /etc/supervisor/conf.d/
+sudo cp /var/www/html/InfrastructureAutomationOnAzure/conf/ecom_supervisor.conf  /etc/supervisor/conf.d/
 sudo supervisorctl reread
 sudo supervisorctl update
 sudo supervisorctl restart all
 
 # Setup Nginx
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo cp /var/www/html/CloudNine/conf/ecom_nginx.conf  /etc/nginx/sites-available/
+sudo cp /var/www/html/InfrastructureAutomationOnAzure/conf/ecom_nginx.conf  /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/ecom_nginx.conf /etc/nginx/sites-enabled/
 
 sudo systemctl restart nginx
